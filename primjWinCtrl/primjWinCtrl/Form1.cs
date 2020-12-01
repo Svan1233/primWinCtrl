f3edfcfe18b4267424f1ccfc7926bbf200c4c568
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-12-01 10:21:56 +0100

    Add files via upload

---
+++ primjWinCtrl/primjWinCtrl/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primjWinCtrl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                System.Diagnostics.Process.Start("notepad.exe");
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                System.Diagnostics.Process.Start("paint.exe");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dali ste sigurni da zelite izaci?", "Potvrda") ;
+        }
+        
+    }
+}
